505b48f948c53fa6e883e490c349ad78f1798544
Dilibe Fidelugwuowo <franklinfidelugwuowo@gmail.com>
2022-04-23 13:28:27 +0100

    Got introduced to combobox

--- Books/Python GUI Programming Cookbook/burkhard_meier.py
+++ Books/Python GUI Programming Cookbook/burkhard_meier.py
@@ -7,18 +7,25 @@ win = tk.Tk()
 win.title("Python GUI")
 
 # Adding a Label
-aLabel = ttk.Label(win, text="Enter a Name:")
-aLabel.grid(column=0, row=0)
+ttk.Label(win, text="Enter a Name:").grid(column=0, row=0)
 
 #Button click Event Callback Function
 def clickMe():
-    action.configure(text="Hello"+name.get())
-    aLabel.configure(foreground='red')
+    action.configure(text="Hello"+name.get()+ '' + numberChosen.get())
 
 # Adding a button
+ttk.Label(win, text="Click Me Section").grid(column=2, row=0)
 action = ttk.Button(win, text='Click Me!', command=clickMe)
-action.grid(column=1, row=1)
-action.configure(state='disabled')
+action.grid(column=2, row=1)
+#action.configure(state='disabled')
+
+# Adding a combobox
+ttk.Label(win, text="Choose a number:").grid(column=1, row=0)
+number = tk.StringVar()
+numberChosen = ttk.Combobox(win, width=12, textvariable=number, state='readonly')
+numberChosen['values'] = (1, 2, 4, 42, 100)
+numberChosen.grid(column=1, row=1)
+numberChosen.current(0)
 
 # Adding a textbox entry widget
 name = tk.StringVar()
