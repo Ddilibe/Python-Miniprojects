bca3d1452d20f9c5e667b9d9b3a5603fcb2f5d0a
Dilibe Fidelugwuowo <franklinfidelugwuowo@gmail.com>
2022-04-23 12:47:13 +0100

    Getting started with the use of the text box widgets

--- Books/Python GUI Programming Cookbook/burkhard_meier.py
+++ Books/Python GUI Programming Cookbook/burkhard_meier.py
@@ -7,6 +7,22 @@ win = tk.Tk()
 win.title("Python GUI")
 
 # Adding a Label
-ttk.Label(win, text="A label").grid(column=0, row=0)
+aLabel = ttk.Label(win, text="Enter a Name:")
+aLabel.grid(column=0, row=0)
+
+#Button click Event Callback Function
+def clickMe():
+    action.configure(text="Hello"+name.get())
+    aLabel.configure(foreground='red')
+
+# Adding a button
+action = ttk.Button(win, text='Click Me!', command=clickMe)
+action.grid(column=1, row=1)
+
+# Adding a textbox entry widget
+name = tk.StringVar()
+nameEntered = ttk.Entry(win, width=12, textvariable=name)
+nameEntered.grid(column=0, row=1)
+
 
 win.mainloop()
