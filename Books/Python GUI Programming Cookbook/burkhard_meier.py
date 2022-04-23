a030ab845e9b89e217c8ae797ccb598ed1015994
Dilibe Fidelugwuowo <franklinfidelugwuowo@gmail.com>
2022-04-23 12:55:52 +0100

    Learnt how to focus on a section and disabling a button

--- Books/Python GUI Programming Cookbook/burkhard_meier.py
+++ Books/Python GUI Programming Cookbook/burkhard_meier.py
@@ -18,11 +18,12 @@ def clickMe():
 # Adding a button
 action = ttk.Button(win, text='Click Me!', command=clickMe)
 action.grid(column=1, row=1)
+action.configure(state='disabled')
 
 # Adding a textbox entry widget
 name = tk.StringVar()
 nameEntered = ttk.Entry(win, width=12, textvariable=name)
 nameEntered.grid(column=0, row=1)
-
+nameEntered.focus()
 
 win.mainloop()
