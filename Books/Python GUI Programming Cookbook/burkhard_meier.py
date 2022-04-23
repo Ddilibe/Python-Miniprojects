fbc940f52a50a70220104fdbd180bd946c6f4d91
Dilibe Fidelugwuowo <franklinfidelugwuowo@gmail.com>
2022-04-23 13:44:06 +0100

    Created a check bitton with different initials states

--- Books/Python GUI Programming Cookbook/burkhard_meier.py
+++ Books/Python GUI Programming Cookbook/burkhard_meier.py
@@ -33,4 +33,20 @@ nameEntered = ttk.Entry(win, width=12, textvariable=name)
 nameEntered.grid(column=0, row=1)
 nameEntered.focus()
 
+# Creating three checkbuttons
+chVarDis = tk.IntVar()
+check1= tk.Checkbutton(win, text='Disabled', variable=chVarDis, state='disabled')
+check1.select()
+check1.grid(column=0, row=4, sticky=tk.W)
+
+chVarUn = tk.IntVar()
+check2 = tk.Checkbutton(win, text="Unchecked", variable=chVarUn)
+check2.deselect()
+check2.grid(column=1, row=4, sticky=tk.W)
+
+chVarEn = tk.IntVar()
+check3 = tk.Checkbutton(win, text="Enabled", variable=chVarEn)
+check3.select()
+check3.grid(column=2, row=4, sticky=tk.W)
+
 win.mainloop()
