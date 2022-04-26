3146a7e11f30c896092044a273764f8b51cac2a8
Dilibe Fidelugwuowo <franklinfidelugwuowo@gmail.com>
2022-04-26 15:42:33 +0100

    Running a test to see whether it works

---
+++ Base Converter/starter.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import ttk, Menu, scrolledtext
+from tkinter import messagebox as mBox
+
+
+class StartBaserClass:
+    """
+    """
+    def __init__(self):
+        """
+        """
+        self._win = tk.Tk()
+        self._win.title("Base Converter")
+        self._win.mainloop()
+
+if __name__ == '__main__':
+    fo = StartBaserClass()
