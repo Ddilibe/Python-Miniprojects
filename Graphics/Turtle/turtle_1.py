f96b8853ed18ff05fde7467df19bf149f666f9ae
Dilibe Fidelugwuowo <franklinfidelugwuowo@gmail.com>
2022-07-13 01:08:42 +0100

    [Feature]: The progress have been quite great

---
+++ Graphics/Turtle/turtle_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import turtle
+import time
+w = turtle.Turtle()
+w.position()
+w.forward(20)
+w.left(90)
+w.forward(30)
+w.left(90)
+w.forward(20)
+w.left(90)
+w.forward(30)
+w.fillcolor('red')
+turtle.mainloop()
