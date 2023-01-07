ff1d9e446159c1d6b1eff3912038fe930eb9872e
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-07 12:15:28 +0100

    [Chore]: Was learning how to use async programming method

---
+++ Graphics/GUI/kivy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+ 
+class TestApp(App):
+    def build(self):
+        return Button(text= " Hello Kivy World ")
+
+if __name__ == "__main__":
+    TestApp().run()
---
+++ Graphics/GUI/tkinter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from tkinter import *
+ 
+class Root(Tk):
+    def __init__(self):
+        super(Root,self).__init__()
+ 
+        self.title("Python Tkinter")
+        self.minsize(500,400)
+ 
+root = Root()
+root.mainloop()
