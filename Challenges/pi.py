d408add9707a9b92a2253b2abb9c3a0c190ba30f
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-19 15:07:56 +0100

    [Chore]: changed some promary location

--- Challenges/pi.py
+++ Challenges/pi.py
@@ -22,18 +22,27 @@ def pi(n: int) -> int:
 			Returns a int value
 
 	"""
-	if type(n) == int:
+	# Verify that n is an integer
+	if n == 0:
+		return 1
+	if type(n) != int:
 		raise TypeError(f"{n} is not an integer")
 	# Section for calculating the numerator
-	two = math.factorial(2*n)
-	numerator = 2*pow(-1, n+1)*two
+	numerator = 2*pow(-1, n+1)*math.factorial(2*n)
 	# Calculating the denominator
-	twon = (pow(2, 2*n))*(bernoulli(2*n))*(1-pow(2, -n))
-	threen = (1-pow(3, -n))*(1-pow(5, -n))*(1-pow(7, -n))
+	twon = (pow(2, 2*n))*(bernoulli(2*n))*(1-pow(2, -2*n))
+	threen = (1-pow(3, -2*n))*(1-pow(5, -2*n))*(1-pow(7, -2*n))
 	denominator = twon * threen
 	first_answer = numerator/denominator
 	ans = pow(first_answer, (1/(2*n)))
-	return ans
+	return (ans)
+
+def dec(n):
+	ans = int(10*(pow(10, n-1)/pi(n-1)))
+	return (ans)
 
 if __name__ == '__main__':
 	print(pi(2))
+	# ans = [pi(i) for i in range(1, 21)]
+	for ans in range(1, 10):
+		print(dec(ans))
